362d5189bb3c8c3871cea7c17a1b1651bbad85fd
WakeUpChang <66504177+WakeUpChang@users.noreply.github.com>
2020-08-05 21:23:17 +0800

    DAY34

--- Homework/Day_034_HW.py
+++ Homework/Day_034_HW.py
@@ -10,10 +10,8 @@ X = np.arange(1000).reshape(200, 5)
 y = np.zeros(200)
 y[:40] = 1
 
-X_train_1, X_test_1, y_train_1, y_test_1 = train_test_split(X[:40], y[:40], test_size=0.25, random_state=10)
-X_train_0, X_test_0, y_train_0, y_test_0 = train_test_split(X[40:200], y[40:200], test_size=1/16, random_state=10)
+X_train_1, X_test_1, y_train_1, y_test_1 = train_test_split(X[:40], y[:40], test_size=10, random_state=10)
+X_train_0, X_test_0, y_train_0, y_test_0 = train_test_split(X[40:200], y[40:200], test_size=10, random_state=10)
 
-X_train = np.append( X_train_1, X_train_0).reshape(180, 5)
-y_train = np.append( y_train_1, y_train_0)
-X_test = np.append( X_test_1, X_test_0).reshape(20, 5)
-y_test = np.append( y_test_1, y_test_0)
+x_train, y_train = np.concatenate([X_train_1, X_train_0]), np.concatenate([y_train_1, y_train_0])
+x_test, y_test = np.concatenate([X_test_1, X_test_0]), np.concatenate([y_test_1, y_test_0])

78d50dcf5dddf09099b04fadc86b4be5a6158d09
WakeUpChang <66504177+WakeUpChang@users.noreply.github.com>
2020-08-19 16:38:13 +0800

    Day50

---
+++ Homework/Day_050_HW.py
@@ -0,0 +1,265 @@
+
+# 做完特徵工程前的所有準備 (與前範例相同)
+import pandas as pd
+import numpy as np
+import copy, time
+import warnings
+warnings.filterwarnings('ignore')
+from IPython.display import display
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
+
+data_path = 'data/'
+df_train = pd.read_csv(data_path + 'titanic_train.csv')
+df_test = pd.read_csv(data_path + 'titanic_test.csv')
+
+train_Y = df_train['Survived']
+ids = df_test['PassengerId']
+df_train = df_train.drop(['PassengerId', 'Survived'] , axis=1)
+df_test = df_test.drop(['PassengerId'] , axis=1)
+df = pd.concat([df_train,df_test])
+df.head()
+
+# 檢查 DataFrame 空缺值的狀態
+def na_check(df_data):
+    data_na = (df_data.isnull().sum() / len(df_data)) * 100
+    data_na = data_na.drop(data_na[data_na == 0].index).sort_values(ascending=False)
+    missing_data = pd.DataFrame({'Missing Ratio' :data_na})
+    display(missing_data.head(10))
+na_check(df)
+
+# 以下 In[3]~In[10] 只是鐵達尼預測中的一組特徵工程, 並以此組特徵工程跑參數, 若更換其他特徵工程, In[10]的參數需要重新跑
+# Sex : 直接轉男 0 女 1
+df["Sex"] = df["Sex"].map({"male": 0, "female":1})
+# Fare : 用 log 去偏態, 0 則直接取 0
+df["Fare"] = df["Fare"].map(lambda i: np.log(i) if i > 0 else 0)
+# Age : 缺值用中位數補
+df["Age"] = df["Age"].fillna(df['Age'].median())
+
+# Title 的 特徵工程 : 將各種頭銜按照類型分類, 最後取 One Hot
+df_title = [i.split(",")[1].split(".")[0].strip() for i in df["Name"]]
+df["Title"] = pd.Series(df_title)
+df["Title"] = df["Title"].replace(['Lady', 'the Countess','Countess','Capt', 'Col','Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+df["Title"] = df["Title"].map({"Master":0, "Miss":1, "Ms" : 1 , "Mme":1, "Mlle":1, "Mrs":1, "Mr":2, "Rare":3})
+df["Title"] = df["Title"].astype(int)
+df = pd.get_dummies(df, columns = ["Title"])
+
+# 新建:家庭大小 (Fsize)特徵, 並依照大小分別建獨立欄位
+df["Fsize"] = df["SibSp"] + df["Parch"] + 1
+df['Single'] = df['Fsize'].map(lambda s: 1 if s == 1 else 0)
+df['SmallF'] = df['Fsize'].map(lambda s: 1 if  s == 2  else 0)
+df['MedF'] = df['Fsize'].map(lambda s: 1 if 3 <= s <= 4 else 0)
+df['LargeF'] = df['Fsize'].map(lambda s: 1 if s >= 5 else 0)
+
+
+# Ticket : 如果不只是數字-取第一個空白之前的字串(去除'.'與'/'), 如果只是數字-設為'X', 最後再取 One Hot
+Ticket = []
+for i in list(df.Ticket):
+    if not i.isdigit() :
+        Ticket.append(i.replace(".","").replace("/","").strip().split(' ')[0])
+    else:
+        Ticket.append("X")        
+df["Ticket"] = Ticket
+df = pd.get_dummies(df, columns = ["Ticket"], prefix="T")
+
+# Cabib 依照第一碼分類, 再取 One Hot
+df["Cabin"] = pd.Series([i[0] if not pd.isnull(i) else 'X' for i in df['Cabin'] ])
+df = pd.get_dummies(df, columns = ["Cabin"], prefix="Cabin")
+
+# Embarked, Pclass 取 One Hot
+df = pd.get_dummies(df, columns = ["Embarked"], prefix="Em")
+df["Pclass"] = df["Pclass"].astype("category")
+df = pd.get_dummies(df, columns = ["Pclass"], prefix="Pc")
+
+# 捨棄 Name 欄位
+df.drop(labels = ["Name"], axis = 1, inplace = True)
+
+# 確認缺值 與 目前的資料表內容
+na_check(df)
+df.head()
+
+# 將資料最大最小化
+df = MinMaxScaler().fit_transform(df)
+
+# 將前述轉換完畢資料 df , 重新切成 train_X, test_X
+train_num = train_Y.shape[0]
+train_X = df[:train_num]
+test_X = df[train_num:]
+
+# 使用三種模型 : 邏輯斯迴歸 / 梯度提升機 / 隨機森林, 參數使用 Random Search 尋找
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+lr = LogisticRegression(tol=0.001, penalty='l2', fit_intercept=True, C=1.0)
+gdbt = GradientBoostingClassifier(tol=100, subsample=0.75, n_estimators=250, max_features=20,
+                                  max_depth=6, learning_rate=0.03)
+rf = RandomForestClassifier(n_estimators=100, min_samples_split=2, min_samples_leaf=1, 
+                            max_features='sqrt', max_depth=6, bootstrap=True)
+
+# 線性迴歸預測檔 (結果有部分隨機, 請以 Kaggle 計算的得分為準, 以下模型同理)
+lr.fit(train_X, train_Y)
+lr_pred = lr.predict_proba(test_X)[:,1]
+sub = pd.DataFrame({'PassengerId': ids, 'Survived': lr_pred})
+sub['Survived'] = sub['Survived'].map(lambda x:1 if x>0.5 else 0) 
+sub.to_csv('titanic_lr.csv', index=False) 
+
+# 梯度提升機預測檔 
+gdbt.fit(train_X, train_Y)
+gdbt_pred = gdbt.predict_proba(test_X)[:,1]
+sub = pd.DataFrame({'PassengerId': ids, 'Survived': gdbt_pred})
+sub['Survived'] = sub['Survived'].map(lambda x:1 if x>0.5 else 0) 
+sub.to_csv('titanic_gdbt.csv', index=False)
+
+# 隨機森林預測檔
+rf.fit(train_X, train_Y)
+rf_pred = rf.predict_proba(test_X)[:,1]
+sub = pd.DataFrame({'PassengerId': ids, 'Survived': rf_pred})
+sub['Survived'] = sub['Survived'].map(lambda x:1 if x>0.5 else 0) 
+sub.to_csv('titanic_rf.csv', index=False)
+
+# 混合泛化預測檔 (依 Kaggle 傳回分數調整比重, 越準確者比重越高, 依資料性質有所不同)
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 19 15:14:13 2020
+
+@author: sandra_chang
+"""
+
+
+# 做完特徵工程前的所有準備 (與前範例相同)
+import pandas as pd
+import numpy as np
+import copy, time
+import warnings
+warnings.filterwarnings('ignore')
+from IPython.display import display
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import LabelEncoder
+
+data_path = 'data/'
+df_train = pd.read_csv(data_path + 'titanic_train.csv')
+df_test = pd.read_csv(data_path + 'titanic_test.csv')
+
+train_Y = df_train['Survived']
+ids = df_test['PassengerId']
+df_train = df_train.drop(['PassengerId', 'Survived'] , axis=1)
+df_test = df_test.drop(['PassengerId'] , axis=1)
+df = pd.concat([df_train,df_test])
+df.head()
+
+# 檢查 DataFrame 空缺值的狀態
+def na_check(df_data):
+    data_na = (df_data.isnull().sum() / len(df_data)) * 100
+    data_na = data_na.drop(data_na[data_na == 0].index).sort_values(ascending=False)
+    missing_data = pd.DataFrame({'Missing Ratio' :data_na})
+    display(missing_data.head(10))
+na_check(df)
+
+# 以下 In[3]~In[10] 只是鐵達尼預測中的一組特徵工程, 並以此組特徵工程跑參數, 若更換其他特徵工程, In[10]的參數需要重新跑
+# Sex : 直接轉男 0 女 1
+df["Sex"] = df["Sex"].map({"male": 0, "female":1})
+# Fare : 用 log 去偏態, 0 則直接取 0
+df["Fare"] = df["Fare"].map(lambda i: np.log(i) if i > 0 else 0)
+# Age : 缺值用中位數補
+df["Age"] = df["Age"].fillna(df['Age'].median())
+
+# Title 的 特徵工程 : 將各種頭銜按照類型分類, 最後取 One Hot
+df_title = [i.split(",")[1].split(".")[0].strip() for i in df["Name"]]
+df["Title"] = pd.Series(df_title)
+df["Title"] = df["Title"].replace(['Lady', 'the Countess','Countess','Capt', 'Col','Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
+df["Title"] = df["Title"].map({"Master":0, "Miss":1, "Ms" : 1 , "Mme":1, "Mlle":1, "Mrs":1, "Mr":2, "Rare":3})
+df["Title"] = df["Title"].astype(int)
+df = pd.get_dummies(df, columns = ["Title"])
+
+# 新建:家庭大小 (Fsize)特徵, 並依照大小分別建獨立欄位
+df["Fsize"] = df["SibSp"] + df["Parch"] + 1
+df['Single'] = df['Fsize'].map(lambda s: 1 if s == 1 else 0)
+df['SmallF'] = df['Fsize'].map(lambda s: 1 if  s == 2  else 0)
+df['MedF'] = df['Fsize'].map(lambda s: 1 if 3 <= s <= 4 else 0)
+df['LargeF'] = df['Fsize'].map(lambda s: 1 if s >= 5 else 0)
+
+
+# Ticket : 如果不只是數字-取第一個空白之前的字串(去除'.'與'/'), 如果只是數字-設為'X', 最後再取 One Hot
+Ticket = []
+for i in list(df.Ticket):
+    if not i.isdigit() :
+        Ticket.append(i.replace(".","").replace("/","").strip().split(' ')[0])
+    else:
+        Ticket.append("X")        
+df["Ticket"] = Ticket
+df = pd.get_dummies(df, columns = ["Ticket"], prefix="T")
+
+# Cabib 依照第一碼分類, 再取 One Hot
+df["Cabin"] = pd.Series([i[0] if not pd.isnull(i) else 'X' for i in df['Cabin'] ])
+df = pd.get_dummies(df, columns = ["Cabin"], prefix="Cabin")
+
+# Embarked, Pclass 取 One Hot
+df = pd.get_dummies(df, columns = ["Embarked"], prefix="Em")
+df["Pclass"] = df["Pclass"].astype("category")
+df = pd.get_dummies(df, columns = ["Pclass"], prefix="Pc")
+
+# 捨棄 Name 欄位
+df.drop(labels = ["Name"], axis = 1, inplace = True)
+
+# 確認缺值 與 目前的資料表內容
+na_check(df)
+df.head()
+
+# 將資料最大最小化
+df = MinMaxScaler().fit_transform(df)
+
+# 將前述轉換完畢資料 df , 重新切成 train_X, test_X
+train_num = train_Y.shape[0]
+train_X = df[:train_num]
+test_X = df[train_num:]
+
+# 使用三種模型 : 邏輯斯迴歸 / 梯度提升機 / 隨機森林, 參數使用 Random Search 尋找
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingClassifier, RandomForestClassifier
+lr = LogisticRegression(tol=0.001, penalty='l2', fit_intercept=True, C=1.0)
+gdbt = GradientBoostingClassifier(tol=100, subsample=0.75, n_estimators=250, max_features=20,
+                                  max_depth=6, learning_rate=0.03)
+rf = RandomForestClassifier(n_estimators=100, min_samples_split=2, min_samples_leaf=1, 
+                            max_features='sqrt', max_depth=6, bootstrap=True)
+
+# 線性迴歸預測檔 (結果有部分隨機, 請以 Kaggle 計算的得分為準, 以下模型同理)
+lr.fit(train_X, train_Y)
+lr_pred = lr.predict_proba(test_X)[:,1]
+sub = pd.DataFrame({'PassengerId': ids, 'Survived': lr_pred})
+sub['Survived'] = sub['Survived'].map(lambda x:1 if x>0.5 else 0) 
+sub.to_csv('titanic_lr.csv', index=False) 
+
+# 梯度提升機預測檔 
+gdbt.fit(train_X, train_Y)
+gdbt_pred = gdbt.predict_proba(test_X)[:,1]
+sub = pd.DataFrame({'PassengerId': ids, 'Survived': gdbt_pred})
+sub['Survived'] = sub['Survived'].map(lambda x:1 if x>0.5 else 0) 
+sub.to_csv('titanic_gdbt.csv', index=False)
+
+# 隨機森林預測檔
+rf.fit(train_X, train_Y)
+rf_pred = rf.predict_proba(test_X)[:,1]
+sub = pd.DataFrame({'PassengerId': ids, 'Survived': rf_pred})
+sub['Survived'] = sub['Survived'].map(lambda x:1 if x>0.5 else 0) 
+sub.to_csv('titanic_rf.csv', index=False)
+
+# 堆疊泛化套件 mlxtend, 需要先行安裝(使用 pip 安裝即可)在執行環境下
+from mlxtend.classifier import StackingClassifier
+
+meta_estimator = GradientBoostingClassifier(tol=100, subsample=0.70, n_estimators=50, 
+                                           max_features='sqrt', max_depth=4, learning_rate=0.3)
+estimators = [
+     ('lr', lr),
+     ('gdbt', gdbt),
+     ('rf',rf)
+ ]
+#stacking = StackingClassifier(estimators =estimators, final_estimator=LogisticRegression)
+stacking = StackingClassifier(classifiers =[lr,gdbt,rf], meta_classifier=meta_estimator)
+
+stacking.fit(train_X, train_Y)
+stacking_pred = stacking.predict(test_X)
+sub = pd.DataFrame({'PassengerId': ids, 'Survived': stacking_pred})
+sub.to_csv('titanic_stacking.csv', index=False)

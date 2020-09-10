8de64ada6eedc37b05025a96f3b15d4aa0850c60
WakeUpChang <66504177+WakeUpChang@users.noreply.github.com>
2020-09-10 17:59:18 +0800

    Day81

---
+++ Homework/Day_081_HW.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 10 17:36:03 2020
+
+@author: sandra_chang
+"""
+import os
+import keras
+from keras.regularizers import l1, l2, l1_l2
+import matplotlib.pyplot as plt
+# Disable GPU
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+train, test = keras.datasets.cifar10.load_data()
+
+## 資料前處理
+def preproc_x(x, flatten=True):
+    x = x / 255.
+    if flatten:
+        x = x.reshape((len(x), -1))
+    return x
+
+def preproc_y(y, num_classes=10):
+    if y.shape[-1] == 1:
+        y = keras.utils.to_categorical(y, num_classes)
+    return y    
+
+x_train, y_train = train
+x_test, y_test = test
+
+# Preproc the inputs
+x_train = preproc_x(x_train)
+x_test = preproc_x(x_test)
+
+# Preprc the outputs
+y_train = preproc_y(y_train)
+y_test = preproc_y(y_test)
+
+def build_mlp(input_shape, Lx, output_units=10, ratio=1e-4):
+    input_layer = keras.layers.Input(input_shape)
+    x = keras.layers.Dense(units=256,activation="relu",kernel_regularizer=Lx(ratio))(input_layer)
+    x = keras.layers.Dense(units=128,activation="relu",kernel_regularizer=Lx(ratio))(x)
+    x = keras.layers.Dense(units=64,activation="relu",kernel_regularizer=Lx(ratio))(x)
+    x = keras.layers.Dense(units=8,activation="relu",kernel_regularizer=Lx(ratio))(x)
+    out = keras.layers.Dense(units=output_units, activation="softmax", name="output")(x)
+    model = keras.models.Model(inputs=[input_layer], outputs=[out])
+    return model
+
+## 超參數設定
+LEARNING_RATE = 1e-3
+EPOCHS = 50
+BATCH_SIZE = 256
+MOMENTUM = 0.95
+LxRegularization = [l1, l2]
+
+results = {}
+"""
+使用迴圈建立不同的帶不同 L1/L2 的模型並訓練
+"""
+for Ln in LxRegularization:
+    keras.backend.clear_session() # 把舊的 Graph 清掉
+    # print("Experiment with Regulizer = %.6f" % (regulizer_ratio))
+    model = build_mlp(input_shape=x_train.shape[1:],Lx = Ln)
+    model.summary()
+    optimizer = keras.optimizers.SGD(lr=LEARNING_RATE, nesterov=True, momentum=MOMENTUM)
+    model.compile(loss="categorical_crossentropy", metrics=["accuracy"], optimizer=optimizer)
+
+    model.fit(x_train, y_train, 
+              epochs=EPOCHS, 
+              batch_size=BATCH_SIZE, 
+              validation_data=(x_test, y_test), 
+              shuffle=True)
+    
+    # Collect results
+    train_loss = model.history.history["loss"]
+    valid_loss = model.history.history["val_loss"]
+    train_acc = model.history.history["acc"]
+    valid_acc = model.history.history["val_acc"]
+    
+    exp_name_tag = "exp-%s" % str(Ln)
+    results[exp_name_tag] = {'train-loss': train_loss,
+                             'valid-loss': valid_loss,
+                             'train-acc': train_acc,
+                             'valid-acc': valid_acc}
+    
+color_bar = ["r", "g", "b", "y", "m", "k"]
+
+plt.figure(figsize=(8,6))
+for i, cond in enumerate(results.keys()):
+    plt.plot(range(len(results[cond]['train-loss'])),results[cond]['train-loss'], '-', label=cond, color=color_bar[i])
+    plt.plot(range(len(results[cond]['valid-loss'])),results[cond]['valid-loss'], '--', label=cond, color=color_bar[i])
+plt.title("Loss")
+plt.ylim([0, 5])
+plt.legend()
+plt.show()
+
+plt.figure(figsize=(8,6))
+for i, cond in enumerate(results.keys()):
+    plt.plot(range(len(results[cond]['train-acc'])),results[cond]['train-acc'], '-', label=cond, color=color_bar[i])
+    plt.plot(range(len(results[cond]['valid-acc'])),results[cond]['valid-acc'], '--', label=cond, color=color_bar[i])
+plt.title("Accuracy")
+plt.legend()
+plt.show()

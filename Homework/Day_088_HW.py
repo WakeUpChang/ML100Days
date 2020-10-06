c06c07abc0bb9f47559c8ae9a3d5028252095f20
WakeUpChang <66504177+WakeUpChang@users.noreply.github.com>
2020-10-06 09:24:56 +0800

    Day88

---
+++ Homework/Day_088_HW.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Work
+# 1. 請嘗試寫一個 callback 用來記錄各類別在訓練過程中，對驗證集的 True Positive 與 True Negative
+
+# In[ ]:
+
+
+import os
+import keras
+
+# Disable GPU
+os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+
+# In[ ]:
+
+
+train, test = keras.datasets.cifar10.load_data()
+
+
+# In[ ]:
+
+
+## 資料前處理
+def preproc_x(x, flatten=True):
+    x = x / 255.
+    if flatten:
+        x = x.reshape((len(x), -1))
+    return x
+
+def preproc_y(y, num_classes=10):
+    if y.shape[-1] == 1:
+        y = keras.utils.to_categorical(y, num_classes)
+    return y    
+
+
+# In[ ]:
+
+
+x_train, y_train = train
+x_test, y_test = test
+
+# Preproc the inputs
+x_train = preproc_x(x_train)
+x_test = preproc_x(x_test)
+
+# Preprc the outputs
+y_train = preproc_y(y_train)
+y_test = preproc_y(y_test)
+
+
+# In[ ]:
+
+
+from keras.layers import BatchNormalization
+
+def build_mlp(input_shape, output_units=10, num_neurons=[512, 256, 128]):
+    input_layer = keras.layers.Input(input_shape)
+    
+    for i, n_units in enumerate(num_neurons):
+        if i == 0:
+            x = keras.layers.Dense(units=n_units, 
+                                   activation="relu", 
+                                   name="hidden_layer"+str(i+1))(input_layer)
+            x = BatchNormalization()(x)
+        else:
+            x = keras.layers.Dense(units=n_units, 
+                                   activation="relu", 
+                                   name="hidden_layer"+str(i+1))(x)
+            x = BatchNormalization()(x)
+    
+    out = keras.layers.Dense(units=output_units, activation="softmax", name="output")(x)
+    
+    model = keras.models.Model(inputs=[input_layer], outputs=[out])
+    return model
+
+
+# In[ ]:
+
+
+## 超參數設定
+LEARNING_RATE = 1e-3
+EPOCHS = 25
+BATCH_SIZE = 1024
+MOMENTUM = 0.95
+
+
+# In[ ]:
+
+
+# 載入 Callbacks
+from keras.callbacks import Callback
+
+# Record_fp_tp
+class Record_tp_tn(Callback):
+    """Code Here
+    撰寫一個紀錄 ture positive 與 true negative 數量的 callbacks    
+    """
+    def on_epoch_end(self, epoch, logs = {}, thres=0.5):
+        logs = logs or {}
+        y_true = self.validation_data[1].argmax(axis=1)
+        y_pred = self.model.predict(self.validation_data[0])
+        y_pred = (y_pred[:, 1] >= thres) * 1
+        
+        
+        logs["valid_tp"]  = sum((y_true==1) & (y_pred==1))
+        logs["valid_tn"]  = sum((y_true==0) & (y_pred==0))
+        
+        
+rec_tptn = Record_tp_tn()
+
+
+# In[ ]:
+
+
+model = build_mlp(input_shape=x_train.shape[1:])
+model.summary()
+optimizer = keras.optimizers.SGD(lr=LEARNING_RATE, nesterov=True, momentum=MOMENTUM)
+model.compile(loss="categorical_crossentropy", metrics=["accuracy"], optimizer=optimizer)
+
+model.fit(x_train, y_train, 
+          epochs=EPOCHS, 
+          batch_size=BATCH_SIZE, 
+          validation_data=(x_test, y_test), 
+          shuffle=True,
+          callbacks=[rec_tptn]
+         )
+
+
+# Collect results
+train_loss = model.history.history["loss"]
+valid_loss = model.history.history["val_loss"]
+train_acc = model.history.history["accuracy"]
+valid_acc = model.history.history["val_accuracy"]
+
+"""Code Here
+將 tp/tn 從訓練完的模型中取出
+"""
+valid_tp = model.history.history['valid_tp']
+valid_tn = model.history.history['valid_tn']
+
+
+# In[ ]:
+
+
+import matplotlib.pyplot as plt
+
+plt.plot(range(len(train_loss)), train_loss, label="train loss")
+plt.plot(range(len(valid_loss)), valid_loss, label="valid loss")
+plt.legend()
+plt.title("Loss")
+plt.show()
+
+plt.plot(range(len(train_acc)), train_acc, label="train accuracy")
+plt.plot(range(len(valid_acc)), valid_acc, label="valid accuracy")
+plt.legend()
+plt.title("Accuracy")
+plt.show()
+
+plt.plot(range(len(valid_tp)), valid_tp, label="valid tp", color="navy")
+plt.plot(range(len(valid_tn)), valid_tn, label="valid tn", color="red")
+plt.legend()
+plt.title("True positives and True Negatives")
+plt.show()
+
